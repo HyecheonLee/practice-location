11e1c7a86cc78d2fee547dd177f84426cdce9791
hclee <rainbow880616@gmail.com>
2022-11-21 22:19:54 +0900

    Initial commit

---
+++ src/main/kotlin/com/hyecheon/practicelocation/PracticeLocationApplication.kt
@@ -0,0 +1,11 @@
+package com.hyecheon.practicelocation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeLocationApplication
+
+fun main(args: Array<String>) {
+    runApplication<PracticeLocationApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyecheon/practicelocation/PracticeLocationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyecheon.practicelocation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeLocationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
